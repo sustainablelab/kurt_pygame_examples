041bb8787534529d6944e13671b1547b5e0c58eb
sustainablelab <sustainablelab@gmail.com>
2024-07-01 09:03:19 -0400

    Add a white rectangle controlled by w,a,s,d

--- example.py
+++ example.py
@@ -7,7 +7,7 @@ Updated example on 2024-07-01
 
 from pathlib import Path
 import pygame
-from pygame import Color
+from pygame import Color, Rect
 import sys
 import atexit
 import random
@@ -16,6 +16,21 @@ def shutdown():
     print("Shutdown")
     pygame.quit()
 
+def make_triangle() -> list:
+    """Return vertices of a triangle to draw right on the screen."""
+    (x,y) = pygame.mouse.get_pos()
+    s = 100
+    r = max(2,s/10)
+    points = [[x - s, y - s],
+              [x - s, y + s],
+              [x + s, y + s]]
+
+    for p in points:
+        p[0] = p[0] + random.uniform(-r,r)
+        p[1] = p[1] + random.uniform(-r,r)
+
+    return points
+
 class Game:
     def __init__(self):
         pygame.init()
@@ -23,6 +38,7 @@ class Game:
         pygame.mouse.set_visible(True)
         self.surf = pygame.display.set_mode((600,500), flags=pygame.RESIZABLE)
         self.clock = pygame.time.Clock()
+        self.pos = [0,0]
 
     def run(self):
         while True: self.game_loop()
@@ -36,22 +52,19 @@ class Game:
                     match event.key:
                         case pygame.K_2: print("2")
                         case pygame.K_q: sys.exit()
+                        case pygame.K_w: self.pos[1] = self.pos[1] - 10
+                        case pygame.K_s: self.pos[1] = self.pos[1] + 10
+                        case pygame.K_d: self.pos[0] = self.pos[0] + 10
+                        case pygame.K_a: self.pos[0] = self.pos[0] - 10
 
         # Make stuff
-        (x,y) = pygame.mouse.get_pos()
-        s = 100
-        r = max(2,s/10)
-        points = [[x - s, y - s],
-                  [x - s, y + s],
-                  [x + s, y + s]]
+        points = make_triangle()
 
-        for p in points:
-            p[0] = p[0] + random.uniform(-r,r)
-            p[1] = p[1] + random.uniform(-r,r)
 
         # Render stuff
         self.surf.fill(Color(0,0,0))
         pygame.draw.polygon(self.surf, Color(0,0,255), points)
+        pygame.draw.rect(self.surf, Color(255,255,255), Rect(self.pos, (10,100)))
         pygame.display.update()
 
         # Clock - regulate how FAST this loop runs
